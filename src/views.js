2f6debc30207680d347a225ab7ba695fe2312814
Muse <muse@local>
2026-09-14 23:23:34 -0500

    Home screen app name: The Daily Hitter

--- src/views.js
+++ src/views.js
@@ -34,7 +34,7 @@ function layout({ title, user, tabs, body }) {
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-<meta name="apple-mobile-web-app-title" content="Daily Hitter">
+<meta name="apple-mobile-web-app-title" content="The Daily Hitter">
 <meta name="theme-color" content="#0a0a0a">
 </head>
 <body>
